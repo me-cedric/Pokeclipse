7f9928d65052028f3f01e458ff9e3f0143cc8de9
Cédric Meyer <mecedricod@gmail.com>
2025-05-29 12:45:06 +0200

    docs(db): Add docker db and documentation

--- README.md
+++ README.md
@@ -4,6 +4,18 @@
 
 Add a `.env` file from one of the examples in `apps/api`.
 
+Run the docker compose file `docker compose up -d` if you don't have a postgres instance ready.
+
+Connect to Pgadmin if you need to check the contents of the database `http://localhost:8888/` and connect to the database server using :
+```
+host: host.docker.internal
+database: <from .env>
+user: <from .env>
+password: <from .env>
+```
+
 `npm i -g turbo`.
+
+`cd apps/api` and `npx prisma generate` and `npx prisma migrate dev --name init`.
 
 `npm run dev` in root directory.
---
+++ apps/api/prisma/migrations/20250529103259_init/migration.sql
@@ -0,0 +1,96 @@
+-- CreateEnum
+CREATE TYPE "GameState" AS ENUM ('IDLE', 'QUESTIONS_1', 'QUESTIONS_2', 'BLIND_TEST_1', 'BLIND_TEST_2', 'WHOS_THAT_1', 'WHOS_THAT_2', 'GEOGUESSR', 'FINAL', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('PLAYER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "session" (
+    "id" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "ipAddress" TEXT,
+    "userAgent" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "account" (
+    "id" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "accessToken" TEXT,
+    "refreshToken" TEXT,
+    "idToken" TEXT,
+    "accessTokenExpiresAt" TIMESTAMP(3),
+    "refreshTokenExpiresAt" TIMESTAMP(3),
+    "scope" TEXT,
+    "password" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verification" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN NOT NULL DEFAULT false,
+    "currentState" "GameState" NOT NULL DEFAULT 'IDLE',
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "GameMember" (
+    "id" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "userType" "UserType" NOT NULL,
+
+    CONSTRAINT "GameMember_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "session_token_key" ON "session"("token");
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
